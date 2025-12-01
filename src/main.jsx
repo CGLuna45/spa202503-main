45807c27e209b24591d7699052d223d6d646876a
CGLuna45 <carlosgustavo.2004@gmail.com>
2025-11-30 20:16:06 -0600

    Fix: Configurar basename de React Router para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from "react-router";
 import './index.css'
 import App from './App.jsx'
 
+// Obtener la ruta base dependiendo del entorno
+const basename = import.meta.env.PROD ? '/spa202503-main/' : '/';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    < BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
